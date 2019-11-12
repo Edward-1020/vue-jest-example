f8cba1f43e945356859064cea49c549ff8b2a70f
Edward-1020 <iezzuyoung@gmail.com>
2019-11-13 07:26:02 +0800

    feat: 样式

--- src/containers/TodoList/components/Header.vue
+++ src/containers/TodoList/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <input
-      data-test="input"
-      v-model="inputValue"
-      @keyup.enter="addTodoItem"
-      type="text"
-    />
+  <div class="header">
+    <div class="header-content">
+      TodoList:
+      <input
+        class="header-input"
+        data-test="input"
+        v-model="inputValue"
+        @keyup.enter="addTodoItem"
+        type="text"
+      />
+    </div>
   </div>
 </template>
 
@@ -28,4 +32,22 @@ export default {
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
+  .header {
+    line-height: 60px;
+    background: #333;
+  }
+  .header-content {
+    width: 600px;
+    margin: 0 auto;
+    color: #fff;
+    font-size: 24px;
+  }
+  .header-input {
+    float: right;
+    width: 360px;
+    margin-top: 16px;
+    line-height: 24px;
+    outline: none;
+    color: #333;
+  }
 </style>
